a96ac3b8e6852bf9feaa695883bd8ef099d07a8c
a9palmer <a9palmer@cs.uwaterloo.ca>
2016-12-18 13:24:14 -0500

    Updates

--- parser.php
+++ parser.php
@@ -26,7 +26,7 @@
     $skipped = 0;
     $allProcessed = [];
 
-    for($i=1; $i<count($fileToParseArray);) {
+    for($i=1; $i<count($fileToParseArray);$i +=2) {
 
         $newInstance = [];
 
@@ -36,6 +36,21 @@
 
             $matched = preg_match("/$cpuTimeRegex/", $fileToParseArray[$i+1], $match);
             $newInstance['cpuTime'] = $match[1];
+
+            $matched = preg_match("/$restartRegex/", $fileToParseArray[$i], $match);
+            $newInstance['restarts'] = $match[1];
+
+            $matched = preg_match("/$conflictRegex/", $fileToParseArray[$i], $match);
+            $newInstance['conflicts'] = $match[1];
+
+            $matched = preg_match("/$decisionsRegex/", $fileToParseArray[$i], $match);
+            $newInstance['decisions'] = $match[1];
+
+            $matched = preg_match("/$propagationsRegex/", $fileToParseArray[$i], $match);
+            $newInstance['propagations'] = $match[1];
+
+            $matched = preg_match("/$conflictLiteralsRegex/", $fileToParseArray[$i], $match);
+            $newInstance['conflictLiterals'] = $match[1];
 
             // Might be redundant, but whatever
             if ( $i%2==1 ) {
@@ -58,21 +73,6 @@
                 $newInstance = array_merge($newInstance, $mapping);
             }
 
-            $matched = preg_match("/$restartRegex/", $fileToParseArray[$i], $match);
-            $newInstance['restarts'] = $match[1];
-
-            $matched = preg_match("/$conflictRegex/", $fileToParseArray[$i], $match);
-            $newInstance['conflicts'] = $match[1];
-
-            $matched = preg_match("/$decisionsRegex/", $fileToParseArray[$i], $match);
-            $newInstance['decisions'] = $match[1];
-
-            $matched = preg_match("/$propagationsRegex/", $fileToParseArray[$i], $match);
-            $newInstance['propagations'] = $match[1];
-
-            $matched = preg_match("/$conflictLiteralsRegex/", $fileToParseArray[$i], $match);
-            $newInstance['conflictLiterals'] = $match[1];
-
             //print_r($newInstance); exit;
 
             // Next index
@@ -85,8 +85,6 @@
         } else {
             $skipped++;
         }
-
-        $i +=2;
 
         if (!empty($newInstance)) {
             //print_r($newInstance);
